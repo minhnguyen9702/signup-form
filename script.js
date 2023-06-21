c88bb102e6d648d4a4b5819deaa6750407e59dcf
minhnguyen9702 <minhnguyen9702@gmail.com>
2023-06-21 13:57:29 +1000

    finish password similar function

--- script.js
+++ script.js
@@ -1,10 +1,18 @@
-const password = document.querySelector("#password")
-const confirm = document.querySelector("#confirm")
 
-function confirmation (password, confirm) {
-    if (password.textContent == confirm.textContent) {
-        return true
+function confirmation() {
+    const password = document.getElementById('password')
+    const confirm = document.getElementById('confirm')
+    if (password.value == "" && confirm.value == "") {
+        password.style.borderColor = "red";
+        confirm.style.borderColor = "red";
+        return console.log("false")
+    } else if (password.value == confirm.value) {
+        password.style.borderColor = "lime";
+        confirm.style.borderColor = "lime";
+        return console.log("true")
     } else {
-        return false
+        password.style.borderColor = "red";
+        confirm.style.borderColor = "red";
+        return console.log("false")
     }
 }
