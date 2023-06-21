fd9e2ffe63ed75727a535458b29fe622d0b7cab4
minhnguyen9702 <minhnguyen9702@gmail.com>
2023-06-21 12:55:31 +1000

    add function to check if password and confirm same

--- script.js
+++ script.js
@@ -1,2 +1,10 @@
 const password = document.querySelector("#password")
 const confirm = document.querySelector("#confirm")
+
+function confirmation (password, confirm) {
+    if (password.textContent == confirm.textContent) {
+        return true
+    } else {
+        return false
+    }
+}
