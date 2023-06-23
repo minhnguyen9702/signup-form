1cb59714aac4add9e7dddda5599dfa46c1b9b01f
minhnguyen9702 <minhnguyen9702@gmail.com>
2023-06-23 16:18:04 +1000

    add error message to password error functionality

--- script.js
+++ script.js
@@ -2,17 +2,18 @@
 function confirmation() {
     const password = document.getElementById('password')
     const confirm = document.getElementById('confirm')
+    const error = document.getElementById('error')
     if (password.value == "" && confirm.value == "") {
         password.style.borderColor = "red";
         confirm.style.borderColor = "red";
-        return console.log("false")
+        error.innerHTML = "✖ Password cannot be empty";
     } else if (password.value == confirm.value) {
         password.style.borderColor = "lime";
         confirm.style.borderColor = "lime";
-        return console.log("true")
+        error.innerHTML = "";
     } else {
         password.style.borderColor = "red";
         confirm.style.borderColor = "red";
-        return console.log("false")
+        error.innerHTML = "✖ Passwords do not match";
     }
 }
